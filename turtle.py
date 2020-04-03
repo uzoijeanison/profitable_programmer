f4eb22c1f12e9b0edd281ebf230780edb1e2281f
uzoijeanison <uzoije.anison@anisontechnologies.com>
2020-04-03 02:46:26 +0000

    created the turtle file

---
+++ turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle = turtle.Turtle()
+
+turtle.forward(90)
+turtle.left(90)
+
+turtle.forward(90)
+turtle.left(90)
+
+turtle.forward(90)
+turtle.left(90)
+
+turtle.forward(90)
+turtle.left(90)
